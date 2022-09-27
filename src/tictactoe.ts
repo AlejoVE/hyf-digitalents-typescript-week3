913126cadac9000b457667dbdc227f4f5ebb963f
Alejandro Montilla <amontilla@luckycycle.com>
2022-09-27 22:00:01 +0200

    Change parameter name & add comments

--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -16,13 +16,13 @@ class Board {
 -------------- --------------  --------------  --------------
         `)
   }
-  public renderBoard(object: Values) {
+  public renderBoard(state: Values) {
     console.log(`
-            ${object['1'] || ' '} | ${object['2'] || ' '} |  ${object['3'] || ' '}
+            ${state['1'] || ' '} | ${state['2'] || ' '} |  ${state['3'] || ' '}
             -----------
-            ${object['4'] || ' '} | ${object['5'] || ' '} |  ${object['6'] || ' '}
+            ${state['4'] || ' '} | ${state['5'] || ' '} |  ${state['6'] || ' '}
             -----------
-            ${object['7'] || ' '} | ${object['8'] || ' '} |  ${object['9'] || ' '}
+            ${state['7'] || ' '} | ${state['8'] || ' '} |  ${state['9'] || ' '}
 
     `)
 
@@ -37,34 +37,34 @@ class Game extends Board {
 
   public command(command: string): void {
 
-    const move: number = Number(command)
+    const move: number = Number(command);
 
     // Check if the move is valid
     if (!this.availableMoves.includes(move)) {
-      console.log('Invalid move')
-      return
+      console.log('Invalid move');
+      return;
     }
 
-    this.executeMove(move, 'X', 'COMPUTER')
+    this.executeMove(move, 'X', 'COMPUTER');
 
     // Execute computer move after 1.5s
     setTimeout(() => {
-      this.computerMove()
+      this.computerMove();
     }, 1500);
   }
 
   public computerMove(): void {
     const randomIndex: number = Math.floor(Math.random() * this.availableMoves.length);
 
-    const move: number = this.availableMoves[randomIndex]
+    const move: number = this.availableMoves[randomIndex];
 
-    this.executeMove(move, 'O', 'HUMAN')
+    this.executeMove(move, 'O', 'HUMAN');
   }
 
   public executeMove(move: number, value: string, nextTurn: string) {
     // If there are no available moves, check if there is a winner or draw.
     if (this.availableMoves.length === 0) {
-      this.checkGame()
+      this.checkGame();
     }
 
     // Take out current move from available moves
@@ -86,7 +86,11 @@ class Game extends Board {
   }
 
   public checkGame(): void {
-    const winningRows: string[] = [
+
+    // I was helped by this article https://antoniomignano.medium.com/node-js-socket-io-express-tic-tac-toe-10cff9108f7
+
+    // Output example: ['OX, 'OOX', 'XXX', ...]
+    const stateValues: string[] = [
       this.currentState['1'] + this.currentState['2'] + this.currentState['3'], //First row
       this.currentState['4'] + this.currentState['5'] + this.currentState['6'], //second row
       this.currentState['7'] + this.currentState['8'] + this.currentState['9'], //Third row
@@ -95,26 +99,28 @@ class Game extends Board {
       this.currentState['3'] + this.currentState['6'] + this.currentState['9'], //Third Colum
       this.currentState['1'] + this.currentState['5'] + this.currentState['9'], //First Diagonal
       this.currentState['7'] + this.currentState['5'] + this.currentState['3'], //Second Diagonal
-    ]
+    ];
 
-    const match: string[] = ['XXX', 'OOO']
+    const winningCombinations: string[] = ['XXX', 'OOO'];
 
-    for (let i = 0; i <= winningRows.length; i++) {
-      if (winningRows[i] === match[1] || winningRows[i] === match[0]) {
-        console.log(`${this.currentTurn} has won!`)
-        this.endGame()
+    // Check if any of the values of the array match a winning combination
+    for (let i = 0; i <= stateValues.length; i++) {
+      if (stateValues[i] === winningCombinations[0] || stateValues[i] === winningCombinations[1]) {
+        console.log(`${this.currentTurn} has won!`);
+        this.endGame();
       }
     }
 
+    // if there are not winners and there are not more available moves, it's a draw.
     if (this.availableMoves.length === 0) {
-      console.log('Draw!')
-      this.endGame()
+      console.log('Draw!');
+      this.endGame();
     }
 
   }
 
   public endGame(): void {
-    process.exit(0)
+    process.exit(0);
   }
 
 }
